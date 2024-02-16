03a51a91310f31160f91ca93f697df08d5ba1233
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-02-16 13:27:32 +0100

    chore: setup landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import DotBackground from '@/components/DotBackground'
+import ThemeProvider from '@/components/ThemeProvider'
+import { Navigation } from '@/components/navigation'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +19,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navigation />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   )
 }
