91ea1f19a884c8f42b0183a8cf6a0668e52774b2
Benjamin Vinterberg <benjaminvinterberg@gmail.com>
2024-02-16 14:18:57 +0100

    feat: add about me section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import DotBackground from '@/components/DotBackground'
+import { AboutMe } from '@/components/about-me'
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
           </h1>
         </DotBackground>
       </section>
+      <AboutMe />
     </div>
   )
 }
